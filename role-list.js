652517ff53d63c146ae4cdca42b6b26f559e6cbb
Ethan Rickaby <ejrickaby@gmail.com>
2020-09-21 13:40:58 +0930

    added role-list

--- role-list.js
+++ role-list.js
@@ -4,13 +4,13 @@ import AWS from "aws-sdk";
 const cognitoidentityserviceprovider  = new AWS.CognitoIdentityServiceProvider();
 
 export const main = handler(async (event, context) => {
-    console.log(process.env.userPoolId)
+    console.log(process.env.userPoolId);
     const params = {
         UserPoolId: process.env.userPoolId
     };
     const result = await cognitoidentityserviceprovider.listUsers(params);
     if ( ! result.Users ) {
-        throw new Error('Users not found.')
+        throw new Error('Users not found.');
     }
-    return result.Users
-})
+    return result.Users;
+});
