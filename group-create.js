fdf00b2818056b706f58cc7289df13ebcf0ed95b
Ethan Rickaby <ejrickaby@gmail.com>
2020-09-23 15:17:56 +0930

    reverted changes and started setup for role creation

--- group-create.js
+++ group-create.js
@@ -1,34 +1,37 @@
-// import handler from './libs/handler-lib';
-// // import * as uuid from 'uuid';
-// import AWS from "aws-sdk";
+import handler from './libs/handler-lib';
+// import * as uuid from 'uuid';
+import AWS from "aws-sdk";
 
-// const cognitoidentityserviceprovider  = new AWS.CognitoIdentityServiceProvider();
-// const iam = new AWS.IAM();
+const cognitoidentityserviceprovider  = new AWS.CognitoIdentityServiceProvider();
+const iam = new AWS.IAM();
 
-// export const main = handler(async (event, context) => {
-//     const data = JSON.parse(event.body);
+export const main = handler(async (event, context) => {
+    const data = JSON.parse(event.body);
 
-//     const iamRoleParams = {
-//         AssumeRolePolicyDocument: ""
-//     }
+    const iamRoleParams = {
+        AssumeRolePolicyDocument: "",
+        RoleName: data.groupName,
+        Description: data.description,
+        Path: '/notes-app'
+    }
 
-//     const params = {
-//         UserPoolId: process.env.userPoolId,
-//         GroupName: data.groupName,
-//         Description: data.description,
+    const params = {
+        UserPoolId: process.env.userPoolId,
+        GroupName: data.groupName,
+        Description: data.description,
 
-//     };
+    };
 
 
 
-//     return await cognitoidentityserviceprovider.adminCreateUser(params).promise()
-//         .then((res) => {
-//             return res;
-//         })
-//         .catch((err) => {
-//             console.log(err);
-//             if (err) {
-//                 throw new Error(`Error creating user.`);
-//             }
-//         });
-// });
+    return await cognitoidentityserviceprovider.adminCreateUser(params).promise()
+        .then((res) => {
+            return res;
+        })
+        .catch((err) => {
+            console.log(err);
+            if (err) {
+                throw new Error(`Error creating user.`);
+            }
+        });
+});
